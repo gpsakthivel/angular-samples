448a68bc5b51481f0af59e9f9684a3c363094a0f
Sakthivel Palani <psakthi24vel@gmail.com>
2023-06-06 12:01:50 +0530

    ngx-daterangepicker-material plugin initial commit

---
+++ ngx-daterangepicker-material-plugin-poc/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { NgxDaterangepickerMd } from 'ngx-daterangepicker-material';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    NgxDaterangepickerMd.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
